6cd76408925ada4cc6faf0c7c457d5b33b7c636c
248197483@qq.com <248197483@qq.com>
2024-02-21 17:27:15 +0800

    新增app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import {createApp} from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
 let app = createApp(App);
 //绑定路由对象!!!
 app.use(router);
+app.use(ElementPlus)
 app.mount('#app')
