08f97435fea9c7bdbf9011a7f7be087a1473fe71
248197483@qq.com <248197483@qq.com>
2024-02-22 17:05:16 +0800

    框架绘制

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <div>home</div>
+  <div class="wl-home">
+    <img src="@/assets/image/warm.jpg" alt="">
+    <p>欢迎使用warm系统</p>
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.wl-home {
+  max-width: calc(100vw - 64px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 28px;
+  letter-spacing: 4px;
+  color: #e86f67;
 
+  img {
+    margin: 300px 0 60px 0;
+  }
+}
 </style>
